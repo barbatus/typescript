09343a206af4ce94676372eecddfbea669e8e790
barbatus <alex.borodach@gmail.com>
2016-02-16 22:54:01 +0800

    Add beta version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  name: 'typescript',
-  version: '0.1.0',
+  name: 'barbatus:typescript',
+  version: '0.1.0-beta.1',
   summary: 'TypeScript for Meteor',
   git: 'https://github.com/barbatus/typescript',
   documentation: 'README.md'
@@ -8,24 +8,24 @@ Package.describe({
 
 Package.registerBuildPlugin({
   name: 'typescript',
-  use: ['typescript-compiler'],
+  use: ['barbatus:typescript-compiler'],
   sources: ['plugin.js']
 });
 
 Package.onUse(function(api) {
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('typescript-compiler');
+  api.use('barbatus:typescript-compiler@0.5.0-beta.1');
 
   api.addFiles(['typescript-helpers.js']);
 
   api.export(['__extends', '__decorate', '__metadata', '__param', '__awaiter']);
 
-  api.imply('promise');
+  api.imply('promise@0.4.8');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('typescript');
+  api.use('barbatus:typescript');
 
   api.addFiles('runtime-tests.ts');
 });
