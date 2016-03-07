bdddc17a3cb8a6b7af9893eebe4b3bbb8edeb639
barbatus <alex.borodach@gmail.com>
2016-03-07 16:58:04 +0800

    Move runtime tests to tests folder Bump versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'barbatus:typescript',
-  version: '0.2.0-beta.6',
+  version: '0.2.0-beta.7',
   summary: 'TypeScript for Meteor',
   git: 'https://github.com/barbatus/typescript',
   documentation: 'README.md'
@@ -14,7 +14,7 @@ Package.registerBuildPlugin({
 
 Package.onUse(function(api) {
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('barbatus:typescript-compiler@0.5.0-beta.5');
+  api.use('barbatus:typescript-compiler@0.5.0-beta.6');
 
   api.addFiles(['typescript-helpers.js']);
 
@@ -27,6 +27,6 @@ Package.onTest(function(api) {
   api.use('tinytest');
   api.use('barbatus:typescript');
 
-  api.addFiles('runtime-tests.ts');
-  api.addFiles('runtime-tests.tsx');
+  api.addFiles('tests/runtime-tests.ts', 'client');
+  api.addFiles('tests/runtime-react-tests.tsx', 'client');
 });
