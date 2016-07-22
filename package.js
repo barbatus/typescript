eb3b8b4c886ab11137d8cae0a481fa2550509bb4
barbatus <alex.borodach@gmail.com>
2016-07-22 21:21:45 +0300

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'barbatus:typescript',
-  version: '0.3.3',
+  version: '0.3.4_1',
   summary: 'TypeScript for Meteor',
   git: 'https://github.com/barbatus/typescript',
   documentation: 'README.md'
@@ -13,12 +13,12 @@ Package.registerBuildPlugin({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.0.1');
+  api.versionsFrom('1.3.1');
 
   api.use('isobuild:compiler-plugin@1.0.0');
-  api.use('barbatus:typescript-compiler@0.6.1');
+  api.use('barbatus:typescript-compiler@0.6.2');
 
-  api.imply('modules@0.6.1');
+  api.imply('modules@0.6.4');
 
   api.imply('barbatus:typescript-runtime@0.1.2');
 });
